1f91682974ba868dd33825ac524baac25561d774
Anit Singh <31277330+anitsingh367@users.noreply.github.com>
2019-09-25 03:41:26 +0530

    Made some minor changes

--- remote seva/main.js
+++ remote seva/main.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.collapsible').forEach(function (elems) {
+        M.Collapsible.init(elems);
+    });
+    document.querySelectorAll('select').forEach(function (elems) {
+        M.FormSelect.init(elems);
+    });
+    document.querySelectorAll('.modal').forEach(function(elems){
+        M.Modal.init(elems);
+    });
+
+});
